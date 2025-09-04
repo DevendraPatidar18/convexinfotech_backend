136b42233a0566d3034e9f06afaa4d53136a763c
Devendra Patidar <patidardevendra994@gmail.com>
2025-09-04 23:20:38 +0530

    04-09-25 setup database

---
+++ src/env.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const FIREBASE_KEY_PATH = process.env.FIREBASE_KEY_PATH;
+export const PORT = process.env.PORT || 3000;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from "express"
+import dotenv from "dotenv";
+dotenv.config();
+import database from "./db/database.js";
 const port = 3000
+
+database()
+
 const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+console.log(process.env.H);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
