c0605a050d4e3a19dddcfd297f922a66d12577e3
Devendra Patidar <patidardevendra994@gmail.com>
2025-09-14 15:19:04 +0530

    config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express"
 import dotenv from "dotenv";
 dotenv.config();
-import database from "./db/database.js";
-import app from "./app.js";
-const port = 3000
+import database from "./src/db/database.js";
+import app from "./src/app.js";
+const port = 3000;
 
 database()
 
@@ -11,7 +11,7 @@ database()
 console.log(process.env.H);
 
 //routes
-import enquiryRouter from "./routes/enquiry.routes.js";
+import enquiryRouter from "./src/routes/enquiry.routes.js";
 
 app.use("/api/v1/enquiries", enquiryRouter);
 
