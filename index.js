ef45a40a4c81b191fbdb9853d99b843d49f26933
Devendra Patidar <patidardevendra994@gmail.com>
2025-09-16 22:49:50 +0530

    Bug fix: Correctimport statment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 3000;
 console.log(process.env.H);
 database()
 //routes
-import enquiryRouter from "../ser/server/src/routes/enquiry.routes.js";
+import enquiryRouter from "./src/routes/enquiry.routes.js";
 
 app.use("/api/v1/enquiries", enquiryRouter);
 
